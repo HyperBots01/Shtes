ce03134d750621414154b6c004b04c19904e81ae
HyperBots01 <81091730+HyperBots01@users.noreply.github.com>
2021-03-28 10:02:59 +0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-var typed = new Typed('.typed', {
-  strings: ["-help", "@Ilya", "Multifonksiyonel Discord Bot", "Popüler Komutlar", "Değiştirilebilir Prefix -prefix", "Şirin :)", "Kullanımı Kolay"],
-  typeSpeed: 80,
-  backSpeed: 70,
-  loop: true,
-  shuffle: true,
-  backDelay: 3000,
-  contentType: 'html'
-});
